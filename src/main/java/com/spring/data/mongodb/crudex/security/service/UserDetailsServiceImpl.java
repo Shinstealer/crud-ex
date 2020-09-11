0fe2ca760a774141b8c12c0d7122edef0e5471a1
shinstealer <coolldd890@gmail.com>
2020-09-11 14:14:36 +0900

    feat : user details and service class

--- src/main/java/com/spring/data/mongodb/crudex/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/spring/data/mongodb/crudex/security/service/UserDetailsServiceImpl.java
@@ -1,5 +1,26 @@
 package com.spring.data.mongodb.crudex.security.service;
 
-public class UserDetailsServiceImpl {
-  
+import com.spring.data.mongodb.crudex.entity.User;
+import com.spring.data.mongodb.crudex.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  @Autowired
+  UserRepository repository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = repository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+    return UserDetailsImpl.build(user);
+  }
+
 }
