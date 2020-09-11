092fb03086f127c8a8cf4eb55603db244d3a3fe4
shinstealer <coolldd890@gmail.com>
2020-09-11 14:15:56 +0900

    feat : object response

--- src/main/java/com/spring/data/mongodb/crudex/payload/response/MessageResponse.java
+++ src/main/java/com/spring/data/mongodb/crudex/payload/response/MessageResponse.java
@@ -1,5 +1,11 @@
 package com.spring.data.mongodb.crudex.payload.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MessageResponse {
   
+  private String message;
 }
